73831d7847909f16071274c9ba8f213b86201496
Luis Rodríguez <luisr150104@gmail.com>
2025-10-12 18:20:41 -0400

    otro

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
-var contactRouter = require("./routes/contact");
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -24,7 +22,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/contacto", contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
